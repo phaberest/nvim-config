5e5552528d604f49deb8833694ac37705cd004d8
Luca Sartori <luca@ohmybugs.com>
2023-11-05 18:36:34 +0100

    Faster notification vanishing and neotest initial setup

--- lua/user/notify.lua
+++ lua/user/notify.lua
@@ -5,7 +5,13 @@ return {
     stages = "slide",
   },
   config = function()
-    vim.notify = require "notify"
+    local notify = require "notify"
+
+    notify.setup {
+      timeout = 1000,
+      render = "compact",
+    }
+    vim.notify = notify
   end,
   event = "VeryLazy",
 }
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,11 +1,37 @@
 return { -- Highlight, edit, and navigate code
   "nvim-treesitter/nvim-treesitter",
   event = "BufReadPost",
+  version = false,
   build = function()
     pcall(require("nvim-treesitter.install").update { with_sync = true })
   end,
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
   dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
+    {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      config = function()
+        -- When in diff mode, we want to use the default
+        -- vim text objects c & C instead of the treesitter ones.
+        local move = require "nvim-treesitter.textobjects.move" ---@type table<string,fun(...)>
+        local configs = require "nvim-treesitter.configs"
+        for name, fn in pairs(move) do
+          if name:find "goto" == 1 then
+            move[name] = function(q, ...)
+              if vim.wo.diff then
+                local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
+                for key, query in pairs(config or {}) do
+                  if q == query and key:find "[%]%[][cC]" then
+                    vim.cmd("normal! " .. key)
+                    return
+                  end
+                end
+              end
+              return fn(q, ...)
+            end
+          end
+        end
+      end,
+    },
     {
       "JoosepAlviste/nvim-ts-context-commentstring",
       event = "VeryLazy",
@@ -143,5 +169,14 @@ return { -- Highlight, edit, and navigate code
         },
       },
     }
+  end,
+  init = function(plugin)
+    -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+    -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+    -- no longer trigger the **nvim-treeitter** module to be loaded in time.
+    -- Luckily, the only thins that those plugins need are the custom queries, which we make available
+    -- during startup.
+    require("lazy.core.loader").add_to_rtp(plugin)
+    require "nvim-treesitter.query_predicates"
   end,
 }
